c73bea6bde2cd564b195b32c07bf73ad964e6198
acorned <acorned@gmail.com>
2017-06-02 03:23:27 +0300

    making dirs in sh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Task #1"
+mkdir bin
 javac -d bin -cp src src/javase01/t01/main/Main.java
 java -cp bin javase01/t01/main/Main
 echo "Task #2"
+mkdir bin/javase01/t02/
 javac -d bin/javase01/t02/ src/javase01/t02/Epsilon.java
 java -cp bin/javase01/t02/ Epsilon 0.003
 
