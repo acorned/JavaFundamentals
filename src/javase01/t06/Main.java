c424627bedb13318e715ba566df22d5668bbba3c
acorned <acorned@gmail.com>
2017-06-08 00:04:03 +0300

    Some base dependencies for task #6

--- src/javase01/t06/Main.java
+++ src/javase01/t06/Main.java
@@ -4,4 +4,10 @@ package javase01.t06;
  * Created by Александр on 06/06/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main");
+        Note notepad = new Note(0, new NoteRecord("Title", "Some text"));
+        System.out.println(notepad.getNotes()[0].text);
+
+    }
 }
--- src/javase01/t06/Note.java
+++ src/javase01/t06/Note.java
@@ -4,17 +4,38 @@ package javase01.t06;
  * Created by acorned on 06.06.17.
  */
 public class Note {
-    NoteRecord[] notes;
+    private NoteRecord[] notes = new NoteRecord[50];
+    private int numberOfRecords;
 
-    public void showNotes(NoteRecord[] notes) {
-        this.notes = notes;
+    Note(int index, NoteRecord note) {
+        this.notes[index] = note;
+    }
+
+    public NoteRecord[] getNotes() {
+        return notes;
+    }
+
+    private void addNote() {
+        //add note
+    }
+
+    private void deleteNote(String header) {
+        //delete note
     }
 }
 
 class NoteRecord {
-    void EditRecord() {
-        System.out.println("Edit record");
+    String header = null;
+    String text = null;
+
+    NoteRecord(String newHeader, String newText) {
+        this.header = newHeader;
+        this.text = newText;
     }
+
+    //  void editRecord(sourcHeader, newHeader, newText) {
+    //      NoteRecord.header =
+    //  }
 
     ;
 }
