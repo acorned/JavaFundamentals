8a96b0db7baa0019f1102291d73c324e11e513ff
acorned <acorned@gmail.com>
2017-06-29 18:31:44 +0300

    updated project structure

--- src/javase01/t06/NoEntryException.java
+++
@@ -1,9 +0,0 @@
-package javase01.t06;
-
-/** Simple exception which means that record wasn't found.
- * @author Alexander Zheludkov
- * @version 1.0
- */
-
-public class NoEntryException extends RuntimeException{};
-
