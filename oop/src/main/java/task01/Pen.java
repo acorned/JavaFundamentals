8c89eb09cef5e97322f630b23e8e2c1943b454b3
acorned <acorned@gmail.com>
2017-06-30 20:20:49 +0300

    Solved Task2.1, updated project dependencies.

--- oop/src/main/java/task01/Pen.java
+++ oop/src/main/java/task01/Pen.java
@@ -1,39 +1,12 @@
 package task01;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Pen {
     private int cost;
     private String color;
 
-    public Pen(int cost, String color) {
-        this.cost = cost;
-        this.color = color;
-    }
-
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-
-        Pen pen = (Pen) object;
-
-        if (cost != pen.cost) return false;
-        if (color != null ? !color.equals(pen.color) : pen.color != null) return false;
-
-        return true;
-    }
-
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + cost;
-        result = 31 * result + (color != null ? color.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public java.lang.String toString() {
-        return "Pen{" +
-                "cost=" + cost +
-                ", color='" + color + '\'' +
-                '}';
-    }
 }
