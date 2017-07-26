4b95ab8c0357aac1dc1db309b0e3c422607707ab
Alexander Zheludkov <acorned@gmail.com>
2017-07-26 03:19:47 +0300

    Imports added.

--- io/src/main/java/webserver/SimpeServer.java
+++ io/src/main/java/webserver/SimpeServer.java
@@ -1,5 +1,10 @@
 package webserver;
 
+import lombok.extern.log4j.Log4j2;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Log4j2
 public class SimpeServer {
     public static void main(String... args) {
         try (ServerSocket ss = new ServerSocket(Integer.parseInt(args[0]))) {
