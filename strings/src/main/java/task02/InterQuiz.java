577df95bb158eb98f2172b6045ade9259171b849
Alexander Zheludkov <acorned@gmail.com>
2017-07-26 21:52:10 +0300

    Task 3.2 - now it works completely.

--- strings/src/main/java/task02/InterQuiz.java
+++ strings/src/main/java/task02/InterQuiz.java
@@ -11,20 +11,49 @@ public class InterQuiz {
 
     private void takeAQuiz(){
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Please choose language (1 - English, 2 - Russian)");
-            String lang = br.readLine();
-            Locale locale = new Locale(
-                    lang.equals("1") ? "en" :
-                            lang.equals("2") ? "ru" :
-                                    "default");
-            System.out.println(locale);
-            bundle = ResourceBundle.getBundle("data", locale);
-            System.out.println(bundle.getObject("intro"));
-            System.out.printf("%s\n%s\n%s\n",
-                    bundle.getObject("question1"),
-                    bundle.getObject("question2"),
-                    bundle.getObject("question3"));
+            while (true) {
+                boolean toExit = false;
+                System.out.println("Please choose language (1 - English, 2 - Russian)");
+                String inputString = br.readLine();
+                Locale locale = new Locale(
+                        inputString.equals("1") ? "en" :
+                                inputString.equals("2") ? "ru" :
+                                        "default");
 
+                bundle = ResourceBundle.getBundle("data", locale);
+
+                System.out.println(bundle.getObject("intro"));
+                System.out.printf("%s\n%s\n%s\n",
+                        bundle.getObject("question1"),
+                        bundle.getObject("question2"),
+                        bundle.getObject("question3"));
+                inputString = br.readLine();
+                switch (inputString) {
+                    case "1":
+                        System.out.println(bundle.getObject("answer1"));
+                        break;
+                    case "2":
+                        System.out.println(bundle.getObject("answer2"));
+                        break;
+                    case "3":
+                        System.out.println(bundle.getObject("answer3"));
+                        break;
+                    default:
+                        System.out.println(bundle.getObject("wrong_number"));
+                }
+                System.out.println(bundle.getObject("continue"));
+                inputString = br.readLine();
+                switch (inputString) {
+                    case "y":
+                    case "д":
+                        break;
+                    case "n":
+                    case "н":
+                        toExit = true;
+                        break;
+                }
+                if (toExit == true) break;
+            }
         } catch (IOException e) {
             System.out.println(e);
         }
